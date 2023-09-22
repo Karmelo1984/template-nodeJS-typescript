f616d02076ddd9d2999bc85e3d59aa4d512ac7ff
Carmelo <79457759+Karmelo1984@users.noreply.github.com>
2023-09-22 18:18:58 +0200

    Actualización

--- src/server.ts
+++ src/server.ts
@@ -2,14 +2,7 @@ import express from 'express';
 export const app = express();
 
 import cors from 'cors';
-
-// Crea una instancia de la aplicación 'express'
-export const app = express();
-
-// Habilita el uso de 'CORS' en la aplicación
 app.use(cors());
-
-// Habilita el uso de JSON en la aplicación para procesar datos en formato JSON en las solicitudes y respuestas.
 app.use(express.json());
 
 // Importa las rutas del servidor
